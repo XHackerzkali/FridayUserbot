c47b60d6cbab792b839d1f754a325ae8b57d2f05
XHackerzkali <64786928+XHackerzkali@users.noreply.github.com>
2020-06-30 22:06:18 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -17,11 +17,11 @@ ALIVE_caption += "`DATABASE STATUS:` **Functional**\n\n"
 ALIVE_caption += "**Current Branch** : `master`\n\n"
 ALIVE_caption += "**Friday OS** : `3.14`\n\n"
 ALIVE_caption += "**Forked By😎** : `@XHackerzKali`\n\n"
-ALIVE_caption += f"**My Boss** : {DEFAULTUSER} \n\n"
+ALIVE_caption += f"**🔥My Boss🔥** : {DEFAULTUSER} \n\n"
 ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
 ALIVE_caption += "**License** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
-ALIVE_caption += "Copyright By [StarkGang@xhackerzkali](GitHub.com/xhackerzkali)\n\n"
-ALIVE_caption += "[Deploy Modded FridayUserbot](https://github.com/xhackerzkali/fridayuserbot)"
+ALIVE_caption += "Copyright By [Modded Friday](GitHub.com/xhackerzkali)\n\n"
+ALIVE_caption += "[😎Deploy Modded FridayUserbot🔥](https://github.com/xhackerzkali/fridayuserbot)"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
