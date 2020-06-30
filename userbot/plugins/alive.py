0e29ace8698f19b51eb832e6c2ee8dc4cf856703
XHackerzkali <64786928+XHackerzkali@users.noreply.github.com>
2020-06-30 21:31:59 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -16,12 +16,12 @@ ALIVE_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n\n"
 ALIVE_caption += "`DATABASE STATUS:` **Functional**\n\n"
 ALIVE_caption += "**Current Branch** : `master`\n\n"
 ALIVE_caption += "**Friday OS** : `3.14`\n\n"
-ALIVE_caption += "**Current Sat** : `StarkGangSat-2.25`\n\n"
+ALIVE_caption += "**Forked By😎** : `@XHackerzKali`\n\n"
 ALIVE_caption += f"**My Boss** : {DEFAULTUSER} \n\n"
 ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
 ALIVE_caption += "**License** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
-ALIVE_caption += "Copyright By [StarkGang@Github](GitHub.com/StarkGang)\n\n"
-ALIVE_caption += "[Deploy FridayUserbot](https://telegra.ph/FRIDAY-06-15)"
+ALIVE_caption += "Copyright By [StarkGang@xhackerzkali](GitHub.com/xhackerzkali)\n\n"
+ALIVE_caption += "[Deploy Modded FridayUserbot](https://github.com/xhackerzkali/fridayuserbot)"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
